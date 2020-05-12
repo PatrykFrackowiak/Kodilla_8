ae8f19c2c20b9aa636bd7396a2297e788deea31e
PatrykFrackowiak <patryk.frackowiak90@gmail.com>
2020-05-12 20:58:13 +0200

    Prepare  for task 10.2

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,53 @@
-import {settings, select} from './settings.js';
+import { settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        thisApp.activatePage(id);
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function(pageId) {
+    const thisApp = this;
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+
+      for(let link of thisApp.navLinks){
+        link.classList.toggle(
+          classNames.nav.active,
+          link.getAttribute('href') == '#' + pageId
+        );
+      }
+    }
+  },
+
   initMenu: function () {
     const thisApp = this;
     for (let product of thisApp.data.products) {
@@ -30,13 +75,14 @@ const app = {
     thisApp.cart = new Cart(cartElem);
 
     thisApp.productList = document.querySelector(select.containerOf.menu);
-    thisApp.productList.addEventListener('add-to-cart', (event) =>{
+    thisApp.productList.addEventListener('add-to-cart', (event) => {
       app.cart.add(event.detail.product);
     });
   },
 
   init: function () {
     const thisApp = this;
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
   },
